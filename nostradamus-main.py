5be6f6ca4f155ddef2e3faedc11251dcf3571af2
Young Joong Kim <yjkimjunior@berkeley.edu>
2016-05-31 17:07:00 +0100

    wooohoo renaming works, now to improve speed and useability

--- nostradamus-main.py
+++ nostradamus-main.py
@@ -2,19 +2,22 @@
 
 import os
 import sys
+import fnmatch
 
 from clarifai.client import ClarifaiApi
 
 images = []
+image_and_tags_dict = {}
+global_path = ""
 
 def tag_images_in_directory(path, api):
 
-	path = path.rstrip(os.sep)
+	global_path = path.rstrip(os.sep)
 	print("Stripped path is: " + path)
 
 	for file_name in os.listdir(path):
 
-		images.append((open(os.path.join(path, file_name), 'rb'), file_name))
+		images.append((open(os.path.join(path, file_name), 'rb+'), file_name))
 	return api.tag_images(images)
 
 
@@ -28,18 +31,13 @@ def format_tags_with_filenames(clarifai_response, images):
 		}
 
 	"""
-	image_and_tags_dict = {}
-
 	i=0
-
 	while i < len(images):
-
-		image_and_tags_dict[str(images[i])] = clarifai_response['results'][i]['result']['tag']['classes'][0:5]
+		image_and_tags_dict[str(images[i][1])] = clarifai_response['results'][i]['result']['tag']['classes'][0:5]
 		i += 1
+	return image_and_tags_dict
 
-	print(image_and_tags_dict)
-
-def rename_images_in_directory_with_tags():
+def rename_images_in_directory_with_tags(image_and_tags_dict, path):
 	"""
 		This should take each key in the output of format_tags_with_filenames()
 
@@ -51,9 +49,12 @@ def rename_images_in_directory_with_tags():
 			- hot_girl_model_young_brunette.jpg
 			- ugly_boy_beast_abomination_awful.jpg
 	"""
+	# print()
+	# print(type(image_and_tags_dict[]))
+	for fname in os.listdir(path):
 
-
-
+		os.rename(path + "/" + fname, path + "/" + str.join("_", image_and_tags_dict[fname]) + ".png")
+		print(fname)
 
 def main(argv):
   if len(argv) > 1:
@@ -68,9 +69,16 @@ def main(argv):
     response = api.tag_image_urls(imageurl)
 
   elif os.path.isdir(imageurl):
-    response = tag_images_in_directory(imageurl, api)
+    tags_list = tag_images_in_directory(imageurl, api)
 
-    format_tags_with_filenames(response, images)
+
+    # this is a list of lists
+    # you get simply something like ['hot', 'girl', 'model', 'young', 'brunette']
+    # so take that, join it as one string with _ between each word 
+    # and rename each file accordingly. 
+
+    response = rename_images_in_directory_with_tags(format_tags_with_filenames(tags_list, images), imageurl)
+
 
   elif os.path.isfile(imageurl):
     with open(imageurl,'rb') as image_file:
